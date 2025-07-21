117a6c951d189f518c829c641039a59e750bcfc1
Martin Jr Powlette <hi@martinjrpowlette.com>
2025-07-20 20:52:11 -0400

    Export createServerInterceptor

--- packages/opentelemetry-instrumentation-connect-node/src/index.ts
+++ packages/opentelemetry-instrumentation-connect-node/src/index.ts
@@ -1,2 +1,4 @@
 export { ConnectNodeInstrumentation } from './instrumentation';
 export type { ConnectNodeInstrumentationConfig } from './types';
+
+export { createServerInterceptor } from './interceptor';
